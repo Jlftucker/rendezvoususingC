cd8a98be1eb94b5fc9de2cbc83fdc28bbf22a4e2
Joshua Tucker <jt565@kent.ac.uk>
2024-09-04 12:29:27 +0100

    Changed plotter to allow for plotting only max values as well

--- RendzvousInC++/data/Plotter.py
+++ RendzvousInC++/data/Plotter.py
@@ -4,7 +4,7 @@ import csv
 
 
 
-def setup_data_arrays(csv_data, num_generations,num_elements):
+def setup_data_arrays_all(csv_data, num_generations,num_elements):
     
     generation_name =np.array([])
     total_num_elements = num_generations*(num_elements-2)
@@ -21,6 +21,31 @@ def setup_data_arrays(csv_data, num_generations,num_elements):
     
    
     element_list = np.arange(0,total_num_elements)
+    
+    return(elements, element_list)
+
+
+def setup_data_arrays_maxval(csv_data, num_generations,num_elements):
+    
+    generation_name =np.array([])
+    total_num_elements = num_generations*(num_elements-2)
+    elements = np.array([])
+    #csv_data is a 2d list where each row represents a generation
+    for i in csv_data:#grab each row
+        element_row = np.array([])
+        gen_name=i[0]#name is the first element
+        j = 1#initialize counter at one to ignore the name
+        np.append(generation_name, gen_name)
+        while j<(num_elements-1):#we want it to be less than the total number because their is a space at the end of the csv file
+            value = float(i[j])
+            element_row = np.append(element_row, value)
+            j =j+1   
+            
+        element = max(element_row)
+        elements =np.append(elements,element)
+        
+   
+    element_list = np.arange(0,num_generations)
     
     return(elements, element_list)
 
